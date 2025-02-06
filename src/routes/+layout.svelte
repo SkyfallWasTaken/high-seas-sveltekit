84a648692dd152ffb86c8a9e1407484f43f0e509
Mahad Kalam <mahadkalam1@proton.me>
2025-02-06 22:06:37 +0000

    Ugh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   const { children } = $props();
 </script>
 
-{#if !page.url.pathname.includes("/wrapped/U")}
+{#if !page.url.pathname.includes("wrapped")}
   <Header />
 {/if}
 <main class="pb-2 h-full">
