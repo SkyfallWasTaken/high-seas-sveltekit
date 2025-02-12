083ccff9896b6da5fe7e50fadddffa0a2ea093fb
Mahad Kalam <mahadkalam1@proton.me>
2025-02-12 19:04:28 +0000

    Make Umami env vars public

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,11 @@
 </script>
 
 <svelte:head>
-  {#if import.meta.env.UMAMI_WEBSITE_ID && import.meta.env.UMAMI_SCRIPT_URL}
+  {#if import.meta.env.VITE_UMAMI_WEBSITE_ID && import.meta.env.VITE_UMAMI_SCRIPT_URL}
     <script
       defer
-      src={import.meta.env.UMAMI_SCRIPT_URL}
-      data-website-id={import.meta.env.UMAMI_WEBSITE_ID}
+      src={import.meta.env.VITE_UMAMI_SCRIPT_URL}
+      data-website-id={import.meta.env.VITE_UMAMI_WEBSITE_ID}
     ></script>
   {/if}
 </svelte:head>
