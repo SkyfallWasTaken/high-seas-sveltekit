27bbf39569ea7e3ffd1b762cf46bb19dfbe5a0c3
Mahad Kalam <mahadkalam1@proton.me>
2025-02-12 19:00:47 +0000

    Add Umami

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,16 @@
   import { RenderScan } from "svelte-render-scan";
 </script>
 
+<svelte:head>
+  {#if import.meta.env.UMAMI_WEBSITE_ID && import.meta.env.UMAMI_SCRIPT_URL}
+    <script
+      defer
+      src={import.meta.env.UMAMI_SCRIPT_URL}
+      data-website-id={import.meta.env.UMAMI_WEBSITE_ID}
+    ></script>
+  {/if}
+</svelte:head>
+
 {#if dev}
   <RenderScan />
 {/if}
